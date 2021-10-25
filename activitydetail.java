17842a06bd15b2c109c9194c9f9cbb3d51061efc
sudhan670 <88775663+sudhan670@users.noreply.github.com>
2021-10-25 19:18:07 +0530

    activity commit

---
+++ activitydetail.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class main(){
+public static void main(String[]args)
+{
+  Scanner sc=new Scanner(System.in);
+  int rollno=34;
+  int semester=3;
+  int noofpaperin=3;
+  int noofprojectin=3;
+  int noofpaperout=2;
+  int noofprojectin=3;
+  int noofcoding=1;
+  int prizewon=2;
+  System.out.println(rollno+" "+semester+" "+noofpaperin+" "+noofpaperou+" "+noofprojectin+" "+noofcoding+" "+prizewon);
+}
+}
